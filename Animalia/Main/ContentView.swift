992abf6dfa8f7e676aa6af912dff5f3c43f6271f
Ridhwankiki <ridhwanr0607@gmail.com>
2024-04-29 11:59:49 +0700

    Implement grid switch to Animal list and Animal grid

--- Animalia/Main/ContentView.swift
+++ Animalia/Main/ContentView.swift
@@ -10,40 +10,83 @@ import SwiftUI
 struct ContentView: View {
     
     let animals: [AnimalModel] = Bundle.main.decode("animals.json")
+    @State private var isGridViewActive = false
+    @State private var gridLayout: [GridItem] = [GridItem(.flexible())]
+    @State private var gridColumn: Int = 1
+    @State private var toolbarIcon: String = "square.grid.2x2"
+    
+    // MARK: - FUNCTION GRID SWITCH
+    func gridSwitch() {
+        
+        withAnimation(.easeIn) {
+            gridLayout = Array(repeating: .init(.flexible()), count: gridLayout.count % 3 + 1)
+            
+            gridColumn = gridLayout.count
+            print("Grid Number: \(gridColumn)")
+        }
+        
+        switch gridColumn {
+        case 1:
+            toolbarIcon = "square.grid.2x2"
+        case 2:
+            toolbarIcon = "square.grid.3x2"
+        case 3:
+            toolbarIcon = "rectangle.grid.1x2"
+        default:
+            toolbarIcon = "square.grid.2x2"
+        }
+    }
     
     var body: some View {
         NavigationStack {
             Group {
-                List{
-                    CoverImageView()
-                        .frame(height: 300)
-                        .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
-                    
-                    
-                    ForEach(animals) { animal in
-                        AnimalListItemView(animal: animal)
+                if !isGridViewActive {
+                    List{
+                        CoverImageView()
+                            .frame(height: 300)
+                            .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+                        
+                        
+                        ForEach(animals) { animal in
+                            AnimalListItemView(animal: animal)
+                        }
+                    }
+                    .listStyle(.plain)
+                } else {
+                    ScrollView(showsIndicators: false) {
+                        LazyVGrid(columns: gridLayout, alignment: .center, spacing: 12) {
+                            ForEach(animals) { animal in
+                                AnimalGridItemView(animal: animal)
+                            }
+                        }
                     }
                 }
-                .listStyle(.plain)
+                
             }
             .navigationTitle("Animalia")
             .toolbar {
                 ToolbarItem(placement: .topBarTrailing) {
                     HStack(spacing: 16) {
+                        
+                        // MARK: - LIST
                         Button {
-                            //: TODO isGridViewActive
+                            print("List View is Activated")
+                            isGridViewActive = false
                         } label: {
                             Image(systemName: "square.fill.text.grid.1x2")
                                 .font(.title2)
-                                .foregroundStyle(.accent)
+                                .foregroundStyle(isGridViewActive ? .primary : Color.accent)
                         }
                         
+                        // MARK: - GRID
                         Button {
-                            //: TODO isGridViewActive
+                            print("Grid view is activated")
+                            isGridViewActive = true
+                            gridSwitch()
                         } label: {
-                            Image(systemName: "square.grid.2x2")
+                            Image(systemName: toolbarIcon)
                                 .font(.title2)
-                                .foregroundStyle(.accent)
+                                .foregroundStyle(isGridViewActive ? Color.accent : .primary)
                         }
                         
                         
@@ -55,5 +98,6 @@ struct ContentView: View {
 }
 
 #Preview {
+    
     ContentView()
 }
--- Animalia/Modules/Screen/CoverImageView.swift
+++ Animalia/Modules/Screen/CoverImageView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct CoverImageView: View {
     
     let coverImages: [CoverImageModel] = Bundle.main.decode("covers.json")
+    @State private var showingSheet = false
+    @State private var selectedCover: CoverImageModel?
     
     var body: some View {
         TabView {
@@ -17,6 +19,16 @@ struct CoverImageView: View {
                 Image(item.name)
                     .resizable()
                     .scaledToFill()
+                    .onTapGesture {
+                        showingSheet.toggle()
+                        selectedCover = item
+                    }
+            }
+            .sheet(isPresented: $showingSheet) {
+                if let selectedCover = selectedCover {
+                    OctagonImageFrame(cover: selectedCover)
+                        .presentationDetents([.medium, .large])
+                }
             }
         }
         .tabViewStyle(PageTabViewStyle())
